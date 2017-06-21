cb3a46afb35bb5153a630dfe4993b081dee69499
Renan Garcia <email@renangarcia.me>
2017-06-21 00:36:55 -0300

    Updates
    - Create fake Rentals in model
    - List Rentals in View
    - Acceptance testing on rental list
    - Install addon ember-cli-tutorial-style
    - Install ember-cli-mirage
    - Create Adapter Application

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -28,7 +28,12 @@ test('should link to contact information.', function(assert) {
   });
 });
 
-test('should list available rentals.', function(assert) {});
+test('should list available rentals.', function(assert) {
+  visit('/');
+  andThen(function() {
+    assert.equal(find('.listing').length, 3, 'should see 3 listings');
+  });
+});
 
 test('should filter the list of rentals by city.', function(assert) {});
 
